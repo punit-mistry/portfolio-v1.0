cb8825751897207f4dc48f31d7dcd3abe6012c53
Punit <Punitmistr@gmail.com>
2024-01-27 23:48:42 +0530

    project card added

---
+++ src/components/project-card.tsx
@@ -0,0 +1,32 @@
+import { FC }  from 'react'
+interface Props {
+    title: string,
+    description:string,
+    techStack:string[],   
+}
+const ProjectCard:FC<Props> = ({title,description,techStack}) => {
+  return (
+    <div className='mt-5 border-2  w-[32%] hover:cursor-pointer  transition-all border-black dark:border-white rounded-lg p-4'>
+        <div className='animate-pulse w-full h-52 bg-gray-500 rounded-md' />
+        <div className='mt-4 font-bold text-xl'>
+            {title}
+        </div>
+        <div className='min-h-32  p-2'>
+            {description}
+        </div>
+        <div className='capitalize text-sm rounded-md flex flex-wrap gap-2'>
+            {techStack.map((res)=>{
+                return(
+                    <>
+                      <div className=" bg-[#4d4d4d] shadow-md font-mono text-white rounded-md px-2 w-fit">{`<${res}/> `}</div>
+                    </>
+                )   
+                
+                
+            })}
+        </div>
+    </div>
+  )
+}
+
+export default ProjectCard
--- src/components/project.tsx
+++ src/components/project.tsx
@@ -1,11 +1,47 @@
-import React from 'react'
+import React from 'react';
+import ProjectCard from "./project-card";
 
-const Project = () => {
-  return (
-    <div className='h-[80vh] px-4 lg:py-[10rem] lg:px-[20rem]'>
-        <div className="text-3xl font-bold font-mono ">{'<'}Projects{'/>'}</div>
-    </div>
-  )
+interface Project {
+  title: string;
+  techStack: string[];
+  description: string;
 }
 
-export default Project
+const ProjectList: React.FC = () => {
+  const projects: Project[] = [
+    {
+      title:'Nutrition',
+      techStack:['react','tailwind','express','Node.JS','react','tailwind','express','Node.JS'],
+      description:'this is the description this is the description this is the description this is the description this is the description this is the description'
+    },
+    {
+      title:'Nutrition',
+      techStack:['react','tailwind','express','Node.JS'],
+      description:'this is the description this is the description this is the description this is the description this is the description this is the description'
+    },
+    {
+      title:'Nutrition',
+      techStack:['react','tailwind','express','Node.JS'],
+      description:'this is the description this is the description this is the description this is the description this is the description this is the description'
+    },
+    {
+      title:'Nutrition',
+      techStack:['react','tailwind','express','Node.JS'],
+      description:'this is the description this is the description this is the description this is the description this is the description this is the description'
+    },
+  ]
+  return (
+    <div className="h-[80vh] px-4 lg:py-[5rem] lg:px-[20rem]">
+      <div className="text-3xl font-bold font-mono ">
+        {"<"}Projects{"/>"}
+      </div>
+      <div className="flex flex-wrap gap-4">
+        {projects.map((project, index) => (
+          <ProjectCard key={index} title={project.title} techStack={project.techStack} description={project.description} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ProjectList;
