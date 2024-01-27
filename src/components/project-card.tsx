9f02b80850a816805ad3c2f4755aede4ad7e9401
Punit <Punitmistr@gmail.com>
2024-01-27 23:56:25 +0530

    card link added

--- src/components/project-card.tsx
+++ src/components/project-card.tsx
@@ -1,32 +1,29 @@
-import { FC }  from 'react'
+import { FC } from "react";
 interface Props {
-    title: string,
-    description:string,
-    techStack:string[],   
+  title: string;
+  description: string;
+  techStack: string[];
 }
-const ProjectCard:FC<Props> = ({title,description,techStack}) => {
+const ProjectCard: FC<Props> = ({ title, description, techStack }) => {
   return (
-    <div className='mt-5 border-2  w-[32%] hover:cursor-pointer  transition-all border-black dark:border-white rounded-lg p-4'>
-        <div className='animate-pulse w-full h-52 bg-gray-500 rounded-md' />
-        <div className='mt-4 font-bold text-xl'>
-            {title}
-        </div>
-        <div className='min-h-32  p-2'>
-            {description}
-        </div>
-        <div className='capitalize text-sm rounded-md flex flex-wrap gap-2'>
-            {techStack.map((res)=>{
-                return(
-                    <>
-                      <div className=" bg-[#4d4d4d] shadow-md font-mono text-white rounded-md px-2 w-fit">{`<${res}/> `}</div>
-                    </>
-                )   
-                
-                
-            })}
-        </div>
+    <div className="mt-5  border-2 w-[350px] hover:cursor-pointer  transition-all border-black dark:border-white rounded-lg p-4">
+      <div className="relative ">
+        <div className="animate-pulse w-full h-52 bg-gray-500 rounded-md" />
+        <div className="absolute top-50 left-50">This is the Link</div>
+      </div>
+      <div className="mt-4 font-bold text-xl">{title}</div>
+      <div className="min-h-32  p-2">{description}</div>
+      <div className="capitalize text-sm rounded-md flex flex-wrap gap-2">
+        {techStack.map((res) => {
+          return (
+            <>
+              <div className=" bg-[#4d4d4d] shadow-md font-mono text-white rounded-md px-2 w-fit">{`<${res}/> `}</div>
+            </>
+          );
+        })}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ProjectCard
+export default ProjectCard;
--- src/components/project.tsx
+++ src/components/project.tsx
@@ -11,7 +11,17 @@ const ProjectList: React.FC = () => {
   const projects: Project[] = [
     {
       title:'Nutrition',
-      techStack:['react','tailwind','express','Node.JS','react','tailwind','express','Node.JS'],
+      techStack:['react','tailwind','express','Node.JS','react','tailwind'],
+      description:'this is the description this is the description this is the description this is the description this is the description this is the description'
+    },
+    {
+      title:'Nutrition',
+      techStack:['react','tailwind','express','Node.JS'],
+      description:'this is the description this is the description this is the description this is the description this is the description this is the description'
+    },
+    {
+      title:'Nutrition',
+      techStack:['react','tailwind','express','Node.JS'],
       description:'this is the description this is the description this is the description this is the description this is the description this is the description'
     },
     {
@@ -31,11 +41,11 @@ const ProjectList: React.FC = () => {
     },
   ]
   return (
-    <div className="h-[80vh] px-4 lg:py-[5rem] lg:px-[20rem]">
+    <div className="mb-4 px-4 lg:py-[5rem] lg:px-[20rem]">
       <div className="text-3xl font-bold font-mono ">
         {"<"}Projects{"/>"}
       </div>
-      <div className="flex flex-wrap gap-4">
+      <div className="flex flex-wrap gap-4 items-center justify-center mt-4">
         {projects.map((project, index) => (
           <ProjectCard key={index} title={project.title} techStack={project.techStack} description={project.description} />
         ))}
